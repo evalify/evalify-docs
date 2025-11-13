9bc77aec959553e611f9615c9f270210ecdf2320
Manasa V <manuvenke06@gmail.com>
2025-11-13 15:35:01 +0530

    chore: Change themes and color schemes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Get Started with Evalify
+            Evalify Docs - Get Started
           </Link>
         </div>
       </div>
